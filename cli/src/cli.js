4f2fae8736cec23f2b15174135a73866e9a5a8d1
Timo Scholz <timo.scholz@veto.party>
2024-03-02 07:56:54 +0100

    More cleanup.

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 import { spawnSync } from 'child_process';
 
-if ((process.env['NODE_OPTIONS'] || '').includes('--loader ts-node')) await import('./runner.js');
+if ((process.env.NODE_OPTIONS || '').includes('--loader ts-node')) await import('./runner.js');
 else {
   const childProcess = spawnSync(process.argv[0], process.argv.slice(1), {
     stdio: 'inherit',
     env: {
       ...process.env,
-      NODE_OPTIONS: [process.env['NODE_OPTIONS'], '--loader ts-node/esm', '--trace-uncaught'].filter((item) => !!item).join(' ')
+      NODE_OPTIONS: [process.env.NODE_OPTIONS, '--loader ts-node/esm', '--trace-uncaught'].filter((item) => !!item).join(' ')
     },
     windowsHide: true
   });
