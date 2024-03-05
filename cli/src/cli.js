9f6849e5fdb7abd0b944a666e4063011fb0a5865
Timo Scholz <timo.scholz@veto.party>
2024-03-05 02:53:04 +0100

    Fixed cli starter.

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -1,12 +1,25 @@
 #!/usr/bin/env node
 import { spawnSync } from 'child_process';
+import { fileURLToPath } from 'url';
+import Path from 'path';
+
+const __dirname = fileURLToPath(Path.dirname(Path.dirname(import.meta.resolve('baum'))));
+
+console.log(__dirname);
+
+process.on('uncaughtException', (error) => {
+  console.log(JSON.stringify(error));
+  throw error;
+})
 
 if ((process.env.NODE_OPTIONS || '').includes('--loader ts-node')) await import('./runner.js');
+
 else {
   const childProcess = spawnSync(process.argv[0], process.argv.slice(1), {
     stdio: 'inherit',
     env: {
       ...process.env,
+      TS_NODE_PROJECT: __dirname,
       NODE_OPTIONS: [process.env.NODE_OPTIONS, '--loader ts-node/esm', '--trace-uncaught'].filter((item) => !!item).join(' ')
     },
     windowsHide: true
