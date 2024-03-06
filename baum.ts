65198427e40f7c3823c6af526c150567dd37466f
Timo Scholz <timo.scholz@veto.party>
2024-03-06 19:59:19 +0100

    Updated baum.
    Co-authored-by: Kevin <FCQCarryOut@users.noreply.github.com>

--- baum.ts
+++ baum.ts
@@ -1,9 +1,10 @@
 import Path from 'path';
 import { fileURLToPath } from 'url';
 import { NPMPackageManager } from '@veto-party/baum__package_manager__npm';
-import { GroupStep, IBaumManagerConfiguration, PKGMStep, ParallelStep } from 'baum';
+import { GroupStep, IBaumManagerConfiguration, IWorkspace, PKGMStep, ParallelStep } from 'baum';
 import FileSystem from 'fs/promises';
 import { CopyStep } from './core/src/implementation/Step/CopyStep.js';
+import semver from 'semver';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = Path.dirname(__filename);
@@ -13,11 +14,7 @@ export default async (baum: IBaumManagerConfiguration) => {
   const pm = new NPMPackageManager();
 
   baum.setPackageManager(pm);
-
-
-
   baum.setRootDirectory(__dirname);
-
 
   const oldFiles: Record<string, any> = {};
 
@@ -31,7 +28,7 @@ export default async (baum: IBaumManagerConfiguration) => {
         await FileSystem.writeFile(givenPath, oldFiles[`${workspace.getName()}-${workspace.getVersion()}`]);
       }
     },
-    execute: async (workspace, pm) => {
+    execute: async (workspace, pm, root) => {
       const givenPath = Path.join(workspace.getDirectory(), 'package.json');
       const file = (await FileSystem.readFile(givenPath)).toString();
 
@@ -45,6 +42,58 @@ export default async (baum: IBaumManagerConfiguration) => {
           jsonFile.types = './dist/index.d.ts';
         }
       }
+
+      const workspaces = await pm.readWorkspace(root);
+
+      const namesToWorkspaces: Record<string, Record<string, IWorkspace>> = {};
+      const namesTVersions = workspaces.reduce<Record<string, string[]>>((prev, current) => {
+        prev[current.getName()] ??= [];
+        namesToWorkspaces[current.getName()][current.getVersion()] = current;
+        prev[current.getName()].push(current.getVersion());
+        return prev;
+      }, {});
+
+
+      Object.values(namesToWorkspaces).forEach(
+        (workspaceMapping) => Object.values(workspaceMapping).sort(
+          (workspaceA, workspaceB) => semver.compare(workspaceA.getVersion(), workspaceB.getVersion()) || semver.compareBuild(workspaceA.getVersion(), workspaceB.getVersion())
+        )
+      );
+
+      const tempVersion = 'v0.0.0-temp';
+
+      const starToVersion = (version: string) => version === "*" ? (process.env.PUBLISH_VERSION ?? tempVersion) : version;
+
+      const toMappedVersion = (name: string, version: string): string => {
+        if (namesToWorkspaces[name][version].isPublishable()) {
+          return process.env.PUBLISH_VERSION ?? 'v0.0.0-temp';
+        }
+
+        if (version.includes('*')) {
+          const versions = Object.values(namesToWorkspaces[name]);
+          const foundVersion = versions.findLast((lookupVersion) => semver.satisfies(starToVersion(lookupVersion.getVersion()), starToVersion(version)) || !lookupVersion.getVersion().includes("*") || (lookupVersion.isPublishable() && lookupVersion.getVersion().includes("*")));
+
+          if (foundVersion) {
+            return toMappedVersion(name, foundVersion?.getVersion());
+          }
+
+          throw new Error("Invalid dependency.");
+        }
+
+        return version;
+      };
+
+      Object.entries(jsonFile.dependencies).forEach(([k, v]) => {
+        if (namesTVersions[k]?.includes(v as any)) {
+          jsonFile.dependencies[k] = toMappedVersion(k, v as string);
+        }
+      });
+
+      Object.entries(jsonFile.devDependencies).forEach(([k, v]) => {
+        if (namesTVersions[k]?.includes(v as any)) {
+          jsonFile.devDependencies[k] = toMappedVersion(k, v as string);
+        }
+      });
 
       jsonFile.version = process.env.PUBLISH_VERSION;
       jsonFile.license = 'MIT';
