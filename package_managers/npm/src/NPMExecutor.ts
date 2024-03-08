08847ac6817bca25d2b26514655c49d95f06766c
Timo Scholz <timo.scholz@veto.party>
2024-03-08 19:41:40 +0100

    Updated npm executor.

--- package_managers/npm/src/NPMExecutor.ts
+++ package_managers/npm/src/NPMExecutor.ts
@@ -1,7 +1,7 @@
 import { ATemplateExecutor, callbackArgs } from "@veto-party/baum__core";
 import ejs from 'ejs';
 
-const ifSet = (variableName: string, templateStr: string) => `<%= ${variableName} !== undefined ? ${ejs.render(templateStr, { [variableName]: variableName })} : "" %>`;
+const ifSet = (variableName: string, templateStr: string) => `<%- ${variableName} ? include('file', { "$$template": "${Buffer.from(templateStr).toString("base64")}"}) : "" %>`;
 
 const raw_commands: Record<keyof callbackArgs, string> = {
     command: `npx <%= package %><%= command %> ${ifSet('parameters', "-- <%= parameters %>")}`,
@@ -9,6 +9,7 @@ const raw_commands: Record<keyof callbackArgs, string> = {
     publish: `npm publish ${ifSet('registry', '--registry=<%= registry %>')} ${ifSet('access', "access=<%= access %>")}`,
     run: `npm run <%= command %> ${ifSet('parameters', '-- <%= parameters %>')}`
 }
+
 const to_object: { [K in keyof callbackArgs]: (...args: callbackArgs[K]) => any } = {
     command: (command, parameters?: string) => ({ command, parameters }),
     install: (type: string) => ({ type }),
@@ -20,7 +21,12 @@ export class NPMExecutor extends ATemplateExecutor {
 
     constructor() {
         super((name, ...rest) => {
-            return ejs.render(raw_commands[name], (to_object[name] as (...args: any[]) => any)(...rest));
+
+            const params = (to_object[name] as (...args: any[]) => any)(...rest);
+
+            return ejs.compile(raw_commands[name], { client: true })(params, undefined, (path, givenData) => {
+                return ejs.render(Buffer.from(givenData!['$$template'], 'base64').toString(), params);
+            });
         });
     }
 
