ed9249d37a26dde6f1321e45f95fb99d6cf24e0a
Timo Scholz <timo.scholz@veto.party>
2024-03-08 18:22:47 +0100

    Added async cache.

--- core/src/implementation/annotation/Cached.ts
+++ core/src/implementation/annotation/Cached.ts
@@ -7,30 +7,59 @@ import { isEqual } from 'lodash';
  * @param async 
  * @returns 
  */
-export const CachedFN = <T extends (...args: any[]) => any>(async: ReturnType<T> extends Promise<any> ? never : false) => {
+export const CachedFN = <T extends (...args: any[]) => any>(async: ReturnType<T> extends Promise<any> ? true : false) => {
     return (_target: any, __propertyKey: string, context: TypedPropertyDescriptor<T>) => {
-
-        if (async) {
-            throw new Error("Only sync for now.");
-        }
-
-        const storage: [Parameters<T>, any][] = [];
 
         const previous = context.value;
 
-        context.value = (function (this: any, ...args: Parameters<T>): ReturnType<T> {
+        if (async) {
+            const storage: [Parameters<T>, any][] = [];
 
-            const currentResult = storage.find((current) => isEqual(current[0], args));
+            let promises: [(value: ReturnType<T>) => any, (error?: any) => any][] = [];
 
-            if (currentResult) {
-                return currentResult[1];
-            }
+            const resolveOrReject = <Index extends 0 | 1>(index: Index) => (value: Parameters<typeof promises[number][typeof index]>[0]) => promises.forEach((promiseTuple) => promiseTuple[index](value));
+
+            context.value = (async function (this: any, ...args: Parameters<T>): Promise<ReturnType<T>> {
+
+                const currentResult = storage.find((current) => isEqual(current[0], args));
+
+                if (currentResult) {
+                    return currentResult[1];
+                }
+
+                if (promises.length > 0) {
+                    return new Promise((resolve, reject) => {
+                        promises.push([resolve, reject]);
+                    });
+                }
+
+                return new Promise<ReturnType<T>>(async (resolve, reject) => {
+                    promises.push([resolve, reject]);
+
+                    (async (): Promise<ReturnType<T>> => {
+                        const result = previous?.bind(this)(...args);
+                        storage.push([args, result]);
+                        return result;
+                    })().then(resolveOrReject(0), resolveOrReject(1));
+                });//.then(resolveOrReject(0), resolveOrReject(1));
+            }) as any;
+        } else {
+            const storage: [Parameters<T>, any][] = [];
+
+            context.value = (function (this: any, ...args: Parameters<T>): ReturnType<T> {
+
+                const currentResult = storage.find((current) => isEqual(current[0], args));
+
+                if (currentResult) {
+                    return currentResult[1];
+                }
 
 
-            const result = previous?.bind(this)(...args);
-            storage.push([args, result]);
-            return result;
-        }) as any;
+                const result = previous?.bind(this)(...args);
+                storage.push([args, result]);
+                return result;
+            }) as any;
+        }
 
         Object.defineProperty(context.value, 'length', {
             value: previous?.length
--- core/test/annotation/Cached.spec.ts
+++ core/test/annotation/Cached.spec.ts
@@ -17,4 +17,27 @@ describe('Should only run once using annotation', () => {
         expect(data.method()).toEqual(1);
         expect(data.method()).toEqual(1);
     });
+
+    it('Should be async', async () => {
+        class Test {
+
+            private counter: number = 0;
+
+            @CachedFN(true)
+            async method() {
+                await new Promise(resolve => setTimeout(resolve, 100));
+                return ++this.counter;
+            }
+        }
+
+        const data = new Test();
+
+        const [ret1, ret2] = await Promise.all([data.method(), data.method()]);
+
+        expect(ret1).toEqual(1);
+        expect(ret2).toEqual(1);
+
+
+        expect(await data.method()).toEqual(1);
+    });
 });
