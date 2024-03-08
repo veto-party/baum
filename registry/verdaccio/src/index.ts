9e2309516e437ab2ab09adf9eccc916f9fbdcafb
Timo Scholz <timo.scholz@veto.party>
2024-03-08 20:18:33 +0100

    Made init step run once.

--- registry/verdaccio/src/index.ts
+++ registry/verdaccio/src/index.ts
@@ -1,13 +1,33 @@
-import { IWorkspace, IExecutablePackageManager, IStep, PKGMStep, GroupStep } from "@veto-party/baum__core";
+import { IWorkspace, IExecutablePackageManager, PKGMStep, GroupStep, RunOnce } from "@veto-party/baum__core";
 import { ARegistryStep, GenericVersionManager, VersionManagerVersionOverride } from "@veto-party/baum__registry";
 import { PrepareStep } from "./implementation/internal/Docker/PrepareStep.js";
 import { StartupStep } from "./implementation/internal/Docker/StartupStep.js";
 import Crypto from 'crypto';
 import portFinder from 'portfinder';
 
+@RunOnce()
+class InitStep extends GroupStep {
+
+    private port?: number;
+
+    async getPort() {
+        this.port ??= await portFinder.getPortPromise();
+        return this.port;
+    }
+
+    async execute(workspace: IWorkspace, packageManager: IExecutablePackageManager, root: string): Promise<void> {
+
+        const hash = Crypto.createHash("sha256").update(root).digest("hex");
+
+        this.addExecutionStep("prepare", new PrepareStep(hash, root));
+        this.addExecutionStep("startup", new StartupStep(hash, (await this.getPort()).toString(), root));
+    }
+}
+
 export class VerdaccioRegistryStep extends ARegistryStep {
 
     private publishStep?: PKGMStep;
+    private initStep = new InitStep([]);
 
     constructor(
         private pinVersion: string,
@@ -18,7 +38,7 @@ export class VerdaccioRegistryStep extends ARegistryStep {
 
     modifyJSON = (json: any) => {
         json.version = json.version ?? GenericVersionManager.MIN_VERSION;
-        delete json.private;
+        delete json.private
     };
 
     getPublishStep(): PKGMStep | undefined {
@@ -27,12 +47,10 @@ export class VerdaccioRegistryStep extends ARegistryStep {
 
     protected async startExecution(workspace: IWorkspace, pm: IExecutablePackageManager, root: string): Promise<void> {
         await super.startExecution(workspace, pm, root);
-        const [port] = await Promise.all([portFinder.getPortPromise()]);
+        await this.initStep.execute(workspace, pm, root);
 
-        const hash = Crypto.createHash("sha256").update(root).digest("hex");
+        const port = await this.initStep.getPort();
 
-        this.addExecutionStep("prepare", new PrepareStep(hash, root));
-        this.addExecutionStep("startup", new StartupStep(hash, port.toString(), root));
         this.publishStep = new PKGMStep((intent) => intent.publish().setRegistry(`${this.dockerAddress}:${port}`).setForcePublic(false).setAuthorization("not-empty"));
     }
 }
