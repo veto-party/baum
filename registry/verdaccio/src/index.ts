4c15a2e345162a58868ac096ffd683f53c38b260
Timo Scholz <timo.scholz@veto.party>
2024-03-09 02:14:18 +0100

    Updated configuration & added experimental logic.

--- registry/verdaccio/src/index.ts
+++ registry/verdaccio/src/index.ts
@@ -1,5 +1,5 @@
 import Crypto from 'crypto';
-import { GroupStep, IExecutablePackageManager, IWorkspace, PKGMStep, RunOnce } from '@veto-party/baum__core';
+import { CachedFN, GroupStep, type IExecutablePackageManager, type IWorkspace, PKGMStep } from '@veto-party/baum__core';
 import { ARegistryStep, GenericVersionManager, NPMRCForSpecifiedRegistryStep, VersionManagerVersionOverride } from '@veto-party/baum__registry';
 import portFinder from 'portfinder';
 import { PrepareStep } from './implementation/internal/Docker/PrepareStep.js';
@@ -17,13 +17,26 @@ class InitStep extends GroupStep {
     return this.port;
   }
 
-  async execute(workspace: IWorkspace, packageManager: IExecutablePackageManager, root: string): Promise<void> {
+  @CachedFN(true)
+  private async init(root: string) {
     const hash = Crypto.createHash('sha256').update(root).digest('hex');
 
     this.addExecutionStep('prepare', new PrepareStep(hash, root));
     this.addExecutionStep('startup', new StartupStep(hash, (await this.getPort()).toString(), root));
+  }
+
+  @CachedFN(true)
+  async execute(workspace: IWorkspace, packageManager: IExecutablePackageManager, root: string): Promise<void> {
+    await this.init(root);
     await super.execute(workspace, packageManager, root);
   }
+
+  @CachedFN(true)
+  async clean(workspace: IWorkspace, packageManager: IExecutablePackageManager, rootDirectory: string): Promise<void> {
+    await super.clean(workspace, packageManager, rootDirectory)
+  }
+
+
 }
 
 export class VerdaccioRegistryStep extends ARegistryStep {
@@ -54,14 +67,21 @@ export class VerdaccioRegistryStep extends ARegistryStep {
     return this.publishStep;
   }
 
-  protected async startExecution(workspace: IWorkspace, pm: IExecutablePackageManager, root: string): Promise<void> {
-
+  @CachedFN(true)
+  private async prepareExecution(): Promise<void> {
     const port = await this.initStep.getPort();
 
     if (this.doInstall) {
       this.initStep.addExecutionStep("modify_npmrc", new NPMRCForSpecifiedRegistryStep(`${this.dockerAddress}:${port}`));
       this.initStep.addExecutionStep("install", new PKGMStep((intent) => intent.install().ci()));
     }
+  }
+
+  protected async startExecution(workspace: IWorkspace, pm: IExecutablePackageManager, root: string): Promise<void> {
+
+    await this.prepareExecution();
+
+    const port = await this.initStep.getPort();
 
     await this.initStep.execute(workspace, pm, root);
     await super.startExecution(workspace, pm, root);
