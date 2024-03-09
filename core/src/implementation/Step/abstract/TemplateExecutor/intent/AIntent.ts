609664d94fccade3b52dba8ed5cdeff1d3a3503b
Timo Scholz <timo.scholz@veto.party>
2024-03-09 09:13:46 +0100

    Missing abstract property.

--- core/src/implementation/Step/abstract/TemplateExecutor/intent/AIntent.ts
+++ core/src/implementation/Step/abstract/TemplateExecutor/intent/AIntent.ts
@@ -2,6 +2,9 @@ import { IStep } from '../../../../../index.js';
 import { IExecutionIntent } from '../../../../../interface/PackageManager/executor/IPackageManagerExecutor.js';
 
 export abstract class AIntent<T extends any[]> implements IExecutionIntent {
+
+  abstract readonly name: string;
+
   abstract toGroup(): T;
 
   getSuccessCodes?: () => number[];
