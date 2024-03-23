7204feb8c64b6becb302afe89d595c18bdafc721
FCQCarryOut <git@carryout.dev>
2024-03-23 19:36:46 +0100

    [testing] helm-00: added dir compare check

--- steps/helm/test/integration/00-simple/00-simple.spec.ts
+++ steps/helm/test/integration/00-simple/00-simple.spec.ts
@@ -4,8 +4,36 @@ import { BaumManager } from '@veto-party/baum__core';
 import { NPMPackageManager } from '@veto-party/baum__package_manager__npm';
 import { HelmGenerator } from '../../../src/HelmGenerator.js';
 import { HelmGeneratorProvider } from '../../../src/HelmGeneratorProvider.js';
+import FileSystem from 'node:fs/promises';
+import FileSystemSync from 'node:fs';
 
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = Path.join(Path.dirname(__filename), '..');
+
+async function compareDirectories(pathA: string, pathB: string) {
+    const filesA = FileSystemSync.readdirSync(pathA);
+    const filesB = FileSystemSync.readdirSync(pathB);
+
+    if (filesA.length !== filesB.length) {
+        return false;
+    }
+
+    for (const file of filesA) {
+        if ((await FileSystem.stat(file)).isDirectory()) {
+            if (!(await compareDirectories(Path.join(pathA, file), Path.join(pathB, file)))) {
+                return false;
+            }
+        } else {
+            const fileA = FileSystemSync.readFileSync(Path.join(pathA, file), 'utf-8');
+            const fileB = FileSystemSync.readFileSync(Path.join(pathB, file), 'utf-8');
+
+            if (fileA !== fileB) {
+                console.error(`File ${file} is different`);
+                return false;
+            }
+        }
+    }
+}
 
 describe('A simple test', () => {
     it('Should run successfully', async () => {
@@ -23,6 +51,10 @@ describe('A simple test', () => {
 
         await baum.run();
 
-        // TODO: check
+        const testDirectory = Path.join(__dirname, '00-simple');
+        const compareResult = await compareDirectories(Path.join(testDirectory, 'helm'), Path.join(testDirectory, 'expected-helm'));
+
+        expect(compareResult).toBe(true);
+
     });
 });
