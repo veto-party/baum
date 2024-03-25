d7180b5a60a78e740ad77bbad84e741ba22eb49f
Timo Scholz <timo.scholz@veto.party>
2024-03-25 17:44:40 +0100

    Formatting.

--- steps/helm/test/integration/utility/compareDirectories.ts
+++ steps/helm/test/integration/utility/compareDirectories.ts
@@ -1,42 +1,42 @@
-import 'colors';
-import Path from 'node:path';
 import FileSystemSync from 'node:fs';
 import FileSystem from 'node:fs/promises';
+import Path from 'node:path';
+import 'colors';
 import * as Diff from 'diff';
 
 export async function compareDirectories(pathA: string, pathB: string) {
-    const filesA = FileSystemSync.readdirSync(pathA);
-    const filesB = FileSystemSync.readdirSync(pathB);
-  
-    if (filesA.length !== filesB.length) {
-      console.error(`File count is different....  ${JSON.stringify(filesA)} ${JSON.stringify(filesB)}`);
-      return false;
-    }
-  
-    for (const file of filesA) {
-      if ((await FileSystem.stat(Path.join(pathA, file))).isDirectory()) {
-        if (!(await compareDirectories(Path.join(pathA, file), Path.join(pathB, file)))) {
-          return false;
-        }
-      } else {
-        const fileA = FileSystemSync.readFileSync(Path.join(pathA, file), 'utf-8');
-        const fileB = FileSystemSync.readFileSync(Path.join(pathB, file), 'utf-8');
+  const filesA = FileSystemSync.readdirSync(pathA);
+  const filesB = FileSystemSync.readdirSync(pathB);
 
-        const diff = Diff.diffLines(fileA, fileB);
-  
-        if (diff.length > 0) {
-          console.error(`File ${file} is different`);
-          diff.forEach((change, lineNo) => {
-            if (change.added) {
-                console.log(`(+) (${file}:${lineNo}): ${change.value}`.bgGreen.white);
-            } else if(change.removed) {
-                console.log(`(-) (${file}:${lineNo}): ${change.value}`.bgRed.white);
-            }
-          });
-          return false;
-        }
+  if (filesA.length !== filesB.length) {
+    console.error(`File count is different....  ${JSON.stringify(filesA)} ${JSON.stringify(filesB)}`);
+    return false;
+  }
+
+  for (const file of filesA) {
+    if ((await FileSystem.stat(Path.join(pathA, file))).isDirectory()) {
+      if (!(await compareDirectories(Path.join(pathA, file), Path.join(pathB, file)))) {
+        return false;
+      }
+    } else {
+      const fileA = FileSystemSync.readFileSync(Path.join(pathA, file), 'utf-8');
+      const fileB = FileSystemSync.readFileSync(Path.join(pathB, file), 'utf-8');
+
+      const diff = Diff.diffLines(fileA, fileB);
+
+      if (diff.length > 0) {
+        console.error(`File ${file} is different`);
+        diff.forEach((change, lineNo) => {
+          if (change.added) {
+            console.log(`(+) (${file}:${lineNo}): ${change.value}`.bgGreen.white);
+          } else if (change.removed) {
+            console.log(`(-) (${file}:${lineNo}): ${change.value}`.bgRed.white);
+          }
+        });
+        return false;
       }
     }
-  
-    return true;
   }
+
+  return true;
+}
