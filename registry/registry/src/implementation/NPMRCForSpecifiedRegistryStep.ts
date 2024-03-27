4dd903bc897a48a312d84582eb3d4592cda5adc8
Timo Scholz <timo.scholz@veto.party>
2024-03-27 20:05:00 +0100

    (BUGFIX): Udated registry logic.

--- registry/registry/src/implementation/NPMRCForSpecifiedRegistryStep.ts
+++ registry/registry/src/implementation/NPMRCForSpecifiedRegistryStep.ts
@@ -1,11 +1,19 @@
-import { ModifyNPMRC, getDependentWorkspaces } from '@veto-party/baum__core';
+import { type IWorkspace, ModifyNPMRC, getDependentWorkspaces } from '@veto-party/baum__core';
 
 export class NPMRCForSpecifiedRegistryStep extends ModifyNPMRC {
   constructor(private registry: string) {
     super(async (workspace, pm, root) => {
       const packages = await pm.readWorkspace(root);
+      const checkedDependents: IWorkspace[] = [];
+      const currentDependentsToCheck = new Set([workspace]);
 
-      const checkedDependents = getDependentWorkspaces(workspace, packages, pm);
+      let currentIndex = 0;
+      do {
+       const newDependents =  getDependentWorkspaces(Array.from(currentDependentsToCheck.values())[currentIndex], packages, pm);
+       checkedDependents.push(...newDependents);
+       newDependents.forEach((dependent) => currentDependentsToCheck.add(dependent));
+      } while (currentDependentsToCheck.size <= currentIndex++);
+
       return `\n${checkedDependents
         .map((dependent) => (dependent.getName().includes('/') && packages.some((givenPackage) => givenPackage.getName() === dependent.getName()) ? `${dependent.getName().substring(0, dependent.getName().indexOf('/'))}:registry=${this.registry}` : ''))
         .filter(Boolean)
