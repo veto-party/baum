47101174ee3f6748be4787a6248cf363ec479af6
Timo Scholz <timo.scholz@veto.party>
2024-03-29 16:57:57 +0100

    Improved compare directories. Updated chart yaml for one test.

--- steps/helm/test/integration/utility/compareDirectories.ts
+++ steps/helm/test/integration/utility/compareDirectories.ts
@@ -3,29 +3,36 @@ import FileSystem from 'node:fs/promises';
 import Path from 'node:path';
 import 'colors';
 import * as Diff from 'diff';
+import uniq from 'lodash.uniq';
 
 export async function compareDirectories(pathA: string, pathB: string) {
   const filesA = FileSystemSync.readdirSync(pathA);
   const filesB = FileSystemSync.readdirSync(pathB);
 
-  if (filesA.length !== filesB.length) {
-    console.error(`File count is different....  ${JSON.stringify(filesA)} ${JSON.stringify(filesB)}`);
-    return false;
-  }
+  let result = false;
 
-  for (const file of filesA) {
+  const files = uniq([filesA, filesB].flat());
+
+
+  for (const file of files) {
+    if (!filesB.includes(file) || !filesA.includes(file)) {
+      console.error(`File is invalid: ${Path.join(pathB, file)}`.red);
+      result = false;
+      continue;
+    }
+
     if ((await FileSystem.stat(Path.join(pathA, file))).isDirectory()) {
       if (!(await compareDirectories(Path.join(pathA, file), Path.join(pathB, file)))) {
-        return false;
+        result = false;
       }
     } else {
       const fileA = FileSystemSync.readFileSync(Path.join(pathA, file), 'utf-8');
       const fileB = FileSystemSync.readFileSync(Path.join(pathB, file), 'utf-8');
 
-      const diff = Diff.diffLines(fileA, fileB);
+      const diff = Diff.diffLines(fileB, fileA);
 
       if (diff.some((value) => value.added || value.removed)) {
-        console.error(`File [${Path.join(pathA, file)}] is different to [${Path.join(pathB, file)}]`);
+        console.error(`File [${Path.join(pathA, file)}] is different to [${Path.join(pathB, file)}]`.yellow);
         diff.forEach((change, lineNo) => {
           if (change.added) {
             console.log(`(+) (${file}:${lineNo}): ${change.value}`.bgGreen.white);
@@ -33,10 +40,10 @@ export async function compareDirectories(pathA: string, pathB: string) {
             console.log(`(-) (${file}:${lineNo}): ${change.value}`.bgRed.white);
           }
         });
-        return false;
+        result = false;
       }
     }
   }
 
-  return true;
+  return result;
 }
