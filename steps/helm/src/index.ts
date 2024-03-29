20a81e0f644d563867c4dd222007fef472a0fc78
Timo Scholz <timo.scholz@veto.party>
2024-03-29 23:04:52 +0100

    Missing index update.

--- steps/helm/src/index.ts
+++ steps/helm/src/index.ts
@@ -1,3 +1,4 @@
 export { HelmGenerator } from './HelmGenerator.js';
 export { HelmGeneratorProvider } from './HelmGeneratorProvider.js';
 export { HelmPacker } from './HelmPacker.js';
+export { HelmDefinitionUpdator } from './HelmDefinitionUpdator.js';
