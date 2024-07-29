41f167c8e55ea3ebb96fce4070f84a7f141bf890
Timo Scholz <timo.scholz@veto.party>
2024-07-29 19:10:29 +0200

    Fixed sub package execution.

--- core/src/implementation/Step/ConditionalStep.ts
+++ core/src/implementation/Step/ConditionalStep.ts
@@ -3,19 +3,30 @@ import type { IExecutablePackageManager } from '../../interface/PackageManager/I
 import type { IWorkspace } from '../../interface/PackageManager/IPackageManager.js';
 
 export class ConditionalStep implements IStep {
+
+  private executedWorkspaces: IWorkspace[] = [];
+
   constructor(
     private step: IStep,
     private condition: (workspace: IWorkspace, pm: IExecutablePackageManager, rootDirectory: string) => boolean | Promise<boolean>
   ) {}
 
+  private checkExecutionDep(workspace: IWorkspace) {
+    const deps = workspace.getDynamicDependents();
+    return deps.some((lookup) => {
+        return this.executedWorkspaces.some((executedWorkspace) => executedWorkspace.getName() === lookup.getName());
+    });
+  }
+
   async execute(workspace: IWorkspace, packageManager: IExecutablePackageManager, rootDirectory: string): Promise<void> {
-    if (await this.condition(workspace, packageManager, rootDirectory)) {
+    if (this.checkExecutionDep(workspace) || await this.condition(workspace, packageManager, rootDirectory)) {
+        this.executedWorkspaces.push(workspace);
       return this.step.execute(workspace, packageManager, rootDirectory);
     }
   }
 
   async clean(workspace: IWorkspace, packageManager: IExecutablePackageManager, rootDirectory: string): Promise<void> {
-    if (await this.condition(workspace, packageManager, rootDirectory)) {
+    if (this.checkExecutionDep(workspace) || await this.condition(workspace, packageManager, rootDirectory)) {
       return this.step.clean(workspace, packageManager, rootDirectory);
     }
   }
--- steps/git_diff/src/implementation/ConditionalGitDiffStep.ts
+++ steps/git_diff/src/implementation/ConditionalGitDiffStep.ts
@@ -3,62 +3,62 @@ import { ConditionalStep, type IStep } from '@veto-party/baum__core';
 import { type SimpleGit, simpleGit } from 'simple-git';
 
 export class ConditionalGitDiffStep extends ConditionalStep {
-  private diffMap = new Map<string, string[]>();
+    private diffMap = new Map<string, string[]>();
 
-  private async ensureGitDiff(root: string): Promise<string[]> {
-    root = Path.resolve(root);
-    if (!this.diffMap.has(root)) {
-      this.diffMap.set(root, await this.getGitDiff(root));
-    }
-
-    return this.diffMap.get(root)!;
-  }
-
-  private async getGitDiff(root: string): Promise<string[]> {
-    if (typeof this.enabled === "boolean" && !this.enabled) {
-        return [];
-    }
-
-
-    const git = simpleGit({
-      baseDir: root
-    });
-
-    if (typeof this.enabled === "function" && !await this.enabled(root, git)) {
-        return [];
-    }
-
-    const raw_changes = await new Promise<string>(async (resolve, reject) => {
-      git.raw(['diff', `HEAD..${await this.targetBranchGetter(root, git)}`, '--name-only'], (err, data) => (err ? reject(err) : resolve(data!)));
-    });
-
-    const line_changes = raw_changes.split('\n').map((str) => str.trim());
-
-    line_changes.pop();
-
-    return line_changes
-      .map((line) => {
-        try {
-          return Path.resolve(Path.join(root, line));
-        } catch (error) {
-          return '';
+    private async ensureGitDiff(root: string): Promise<string[]> {
+        root = Path.resolve(root);
+        if (!this.diffMap.has(root)) {
+            this.diffMap.set(root, await this.getGitDiff(root));
         }
-      })
-      .filter(Boolean);
-  }
 
-  constructor(
-    step: IStep,
-    private targetBranchGetter: (root: string, git: SimpleGit) => string | Promise<string>,
-    private enabled: boolean|((root: string, git: SimpleGit) => boolean|Promise<boolean>) = true
-  ) {
-    super(step, async (workspace, _pm, rootDirectory) => {
+        return this.diffMap.get(root)!;
+    }
 
-      const path = Path.resolve(workspace.getDirectory());
-      const diff = await this.ensureGitDiff(rootDirectory);
+    private async getGitDiff(root: string): Promise<string[]> {
+        if (typeof this.enabled === "boolean" && !this.enabled) {
+            return [];
+        }
 
-      console.log(diff, path);
-      return diff.some((file) => file.startsWith(path));
-    });
-  }
+
+        const git = simpleGit({
+            baseDir: root
+        });
+
+        if (typeof this.enabled === "function" && !await this.enabled(root, git)) {
+            return [];
+        }
+
+        const raw_changes = await new Promise<string>(async (resolve, reject) => {
+            git.raw(['diff', `HEAD..${await this.targetBranchGetter(root, git)}`, '--name-only'], (err, data) => (err ? reject(err) : resolve(data!)));
+        });
+
+        const line_changes = raw_changes.split('\n').map((str) => str.trim());
+
+        line_changes.pop();
+
+        return line_changes
+            .map((line) => {
+                try {
+                    return Path.resolve(Path.join(root, line));
+                } catch (error) {
+                    return '';
+                }
+            })
+            .filter(Boolean);
+    }
+
+    constructor(
+        step: IStep,
+        private targetBranchGetter: (root: string, git: SimpleGit) => string | Promise<string>,
+        private enabled: boolean | ((root: string, git: SimpleGit) => boolean | Promise<boolean>) = true
+    ) {
+        super(step, async (workspace, _pm, rootDirectory) => {
+
+            const path = Path.resolve(workspace.getDirectory());
+            const diff = await this.ensureGitDiff(rootDirectory);
+
+            console.log(diff, path);
+            return diff.some((file) => file.startsWith(path));
+        });
+    }
 }
