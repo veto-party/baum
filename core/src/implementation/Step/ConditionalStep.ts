fa2ce7f9282262a0f0d56efc1af67a5c8d11e1e9
Timo Scholz <timo.scholz@veto.party>
2024-07-29 17:32:44 +0200

    Linting.

--- core/src/implementation/Step/ConditionalStep.ts
+++ core/src/implementation/Step/ConditionalStep.ts
@@ -1,23 +1,22 @@
-import { IStep } from "../../interface/IStep.js";
-import { IExecutablePackageManager } from "../../interface/PackageManager/IExecutablePackageManager.js";
-import { IWorkspace } from "../../interface/PackageManager/IPackageManager.js";
+import type { IStep } from '../../interface/IStep.js';
+import type { IExecutablePackageManager } from '../../interface/PackageManager/IExecutablePackageManager.js';
+import type { IWorkspace } from '../../interface/PackageManager/IPackageManager.js';
 
 export class ConditionalStep implements IStep {
+  constructor(
+    private step: IStep,
+    private condition: (workspace: IWorkspace, pm: IExecutablePackageManager, rootDirectory: string) => boolean | Promise<boolean>
+  ) {}
 
-    constructor(
-        private step: IStep,
-        private condition: (workspace: IWorkspace, pm: IExecutablePackageManager, rootDirectory: string) => boolean|Promise<boolean> 
-    ) {}
-
-    async execute(workspace: IWorkspace, packageManager: IExecutablePackageManager, rootDirectory: string): Promise<void> {
-        if (await this.condition(workspace, packageManager, rootDirectory)) {
-            return this.step.execute(workspace, packageManager, rootDirectory);
-        }
+  async execute(workspace: IWorkspace, packageManager: IExecutablePackageManager, rootDirectory: string): Promise<void> {
+    if (await this.condition(workspace, packageManager, rootDirectory)) {
+      return this.step.execute(workspace, packageManager, rootDirectory);
     }
+  }
 
-    async clean(workspace: IWorkspace, packageManager: IExecutablePackageManager, rootDirectory: string): Promise<void> {
-        if (await this.condition(workspace, packageManager, rootDirectory)) {
-            return this.step.clean(workspace, packageManager, rootDirectory);
-        }
+  async clean(workspace: IWorkspace, packageManager: IExecutablePackageManager, rootDirectory: string): Promise<void> {
+    if (await this.condition(workspace, packageManager, rootDirectory)) {
+      return this.step.clean(workspace, packageManager, rootDirectory);
     }
+  }
 }
