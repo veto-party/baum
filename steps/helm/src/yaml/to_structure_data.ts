cc14471f3e1ac68c216fa106345295becd46b1c3
Timo Scholz <timo.scholz@veto.party>
2024-08-24 17:46:58 +0200

    Fixed undefined in helm generator.

--- steps/helm/src/yaml/to_structure_data.ts
+++ steps/helm/src/yaml/to_structure_data.ts
@@ -11,5 +11,9 @@ export const to_structured_data = (value: any): ObjectToken | ArrayToken => {
     return new ArrayToken(value.map(to_structured_data));
   }
 
-  return new ObjectToken(Object.fromEntries(Object.entries(value).map(([k, v]) => [k, to_structured_data(v)] as const)));
+  return new ObjectToken(Object.fromEntries(Object.entries(value).map(([k, v]) => {
+    return [k, to_structured_data(v)] as const;
+  }).filter(([, value]) => {
+    return value !== undefined;
+  })));
 };
