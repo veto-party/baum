eebbd1b0e60740468042ebe1dd90d1c1190c66a2
Timo Scholz <timo.scholz@veto.party>
2024-09-03 18:53:41 +0200

    Updated helm generator.

--- steps/helm/src/HelmGenerator.ts
+++ steps/helm/src/HelmGenerator.ts
@@ -134,7 +134,7 @@ export class HelmGenerator implements IStep {
         allBindings
           .filter(([, value]) => !value.static && value.secret && value.is_global)
           .map(([key, value]) => {
-            return [key, new RawToken(`{{.Values.${HelmGenerator.buildVariablePath(value.referenced, '.')} | quote }}`)];
+            return [key, new RawToken(`{{.Values.${value.is_global ? 'global.' : ''}${HelmGenerator.buildVariablePath(value.referenced, '.')} | quote }}`)];
           })
       )
     };
