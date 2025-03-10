f4308019497434440df22e5e739b6b153103c5a5
Timo Scholz <timo.scholz@veto.party>
2025-03-10 20:49:01 +0100

    Update group feature type to support arrays.

--- steps/installer/features/src/abstract/GroupFeature/GroupFeature.ts
+++ steps/installer/features/src/abstract/GroupFeature/GroupFeature.ts
@@ -3,19 +3,16 @@ import { AFeature } from "../AFeature.js";
 import { FromSchema } from "json-schema-to-ts";
 import { cloneDeep, get, set, merge, toPath } from "lodash-es";
 
-type ToDefinitionStructure<Path extends string|undefined, Target> =   
+type MakeTuple<T, N extends number = 1, Items extends any[] = []> = N extends Items['length'] ? [...Items, T] : MakeTuple<T, N, [{}, ...Items]>;
+ 
+type ToDefinitionStructure<Path extends string|undefined, Target> =
     // No path is given, return target
     Path extends undefined ? never: 
+    Path extends '' ? Target :
     // Path is given and contains . : return Key value pair recurisive 
-    Path extends `${infer U}.${infer Rest}` ? {
-        type: 'object';
-        properties: Record<U, ToDefinitionStructure<Rest, Target>>
-        additionalProperties: false;
-    }:
+    Path extends `${infer U}.${infer Rest}` ? Record<U, ToDefinitionStructure<Rest, Target>> :
     // Path is given contain contains array access : return never, since we do not want to allow array access (for now).
-    Path extends `${infer U}[${infer index}]${infer Rest}` ? {
-        type: 'object',
-    } :
+    Path extends `[${infer index}]${infer Rest}` ? index extends number ? MakeTuple<ToDefinitionStructure<Rest, Target>, index> : never :
     Path extends string ? {
         type: 'object';
         properties: Record<Path, Target>;
