c39405292907c682e9b339d0335cb561a42f50e5
Timo Scholz <timo.scholz@veto.party>
2025-03-14 02:44:48 +0100

    Update exports.

--- steps/installer/features/src/index.ts
+++ steps/installer/features/src/index.ts
@@ -1,15 +1,25 @@
-export { AFeature } from './abstract/AFeature.js';
-export { AMergeFeature } from './abstract/AMergeFeature/AMergeFeature.js';
-export type { IFeature } from './interface/IFeature.js';
+
+export type { ToDefinitionStructure } from './interface/types/ToDefinitionStructure.js';
+
+export type { IFeature, IngestResult } from './interface/IFeature.js';
 export type { FeatureContainer } from './interface/IFeatureContainer.js'
+
+export { AFeature } from './abstract/AFeature.js';
+
+export { AMergeFeature } from './abstract/AMergeFeature/AMergeFeature.js';
+export { AKeyOverrideFeature } from './abstract/AMergeFeature/AKeyOverride/AKeyOverride.js'
 
 export { GroupFeature } from './abstract/GroupFeature/GroupFeature.js';
 
-export { ServiceFeature } from './implementation/Service/ServiceFeature.js';
 
-export { VariableFeature } from './implementation/Variable/Variable.js';
 export { BindingFeature } from './implementation/Binding/Binding.js';
-
+export { ExposeFeature } from './implementation/Expose/Expose.js';
+export { NetworkFeature } from './implementation/Network/NetworkFeature.js';
+export { ScalingFeature } from './implementation/Scaling/ScalingFeature.js';
+export { ServiceFeature } from './implementation/Service/ServiceFeature.js';
+export { SystemUsageFeature } from './implementation/SystemUsageFeature/SystemUsageFeature.js';
+export { UpdateStrategy } from './implementation/UpdateStrategyFeature/UpdateStrategyFeature.js';
+export { VariableFeature } from './implementation/Variable/Variable.js';
 
 export { BaseInstaller } from './BaseInstaller.js';
 
