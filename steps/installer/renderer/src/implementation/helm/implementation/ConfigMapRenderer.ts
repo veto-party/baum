47b877dfa856c9b94e20b49c361903c3a9bde395
Timo Scholz <timo.scholz@veto.party>
2025-03-25 20:35:15 +0100

    Linting.

--- steps/installer/renderer/src/implementation/helm/implementation/ConfigMapRenderer.ts
+++ steps/installer/renderer/src/implementation/helm/implementation/ConfigMapRenderer.ts
@@ -6,56 +6,6 @@ import { to_structured_data } from '../yaml/to_structured_data.js';
 
 export class ConfigMapRenderer implements IConfigMapRenderer {
   render<Key>(workspace: IWorkspace | undefined, key: Key | undefined, map: Map<string | undefined, Map<Key | undefined, IConfigMapStructure>>, binding: Map<string, string> | undefined, name: string): IConfigMapRendererResult | Promise<IConfigMapRendererResult> {
-    const allItems: IConfigMapStructure = new Map();
-
-    const entriesToCheck = [Array.from(binding?.entries() ?? [])];
-    const globalsToCheck = new Set<(typeof entriesToCheck)[number]>();
-
-    do {
-      const entries = entriesToCheck.pop()!;
-      const possibleGlobalEntries: (typeof entriesToCheck)[number] = [];
-
-      for (const entry of entries) {
-        const [_key, lookupKey] = entry;
-        const item = map.get(undefined)?.get(key)?.get(lookupKey);
-
-        if (!item) {
-          possibleGlobalEntries.push(entry);
-          globalsToCheck.add(possibleGlobalEntries);
-          continue;
-        }
-
-        allItems.set(_key, item);
-
-        if (item.binding) {
-          entriesToCheck.push(Object.entries(item.binding));
-        }
-      }
-    } while (entriesToCheck.length > 0);
-
-    do {
-      const [entries] = globalsToCheck.values().take(1);
-      globalsToCheck.delete(entries);
-
-      for (const entry of entries) {
-        const [_key, lookupKey] = entry;
-        const item = map.get(undefined)?.get(key)?.get(lookupKey);
-
-        if (!item) {
-          throw new Error(`Entry(${key}, ${lookupKey} not found, checked scoped and global context.`);
-        }
-
-        allItems.set(_key, {
-          ...item,
-          type: 'global'
-        });
-
-        if (item.binding) {
-          globalsToCheck.add(Object.entries(item.binding));
-        }
-      }
-    } while (globalsToCheck.size > 0);
-
     const yaml = {
       apiVersion: 'v1',
       kind: 'ConfigMap',
