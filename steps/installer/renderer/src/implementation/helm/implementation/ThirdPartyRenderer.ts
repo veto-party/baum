04e8ba00681ac3a064b34b643790a679785d0638
Timo Scholz <timo.scholz@veto.party>
2025-04-03 17:58:47 +0200

    Node fix.

--- steps/installer/renderer/src/implementation/helm/implementation/ThirdPartyRenderer.ts
+++ steps/installer/renderer/src/implementation/helm/implementation/ThirdPartyRenderer.ts
@@ -11,7 +11,7 @@ export class ThirdPartyRenderer implements I3rdPartyRenderer {
         const name = await workspace === undefined ? undefined : resolver.getNameByWorkspace(workspace);
 
         return HelmRenderer.mergeElements(
-          new Map(dependencies.entries().flatMap(([key, values]) => Object.entries(values.properties ?? {}).map(([baseKey, value]) => [[name, key, baseKey].filter(Boolean).join('.'), value] as const))),
+          new Map(Array.from(dependencies.entries()).flatMap(([key, values]) => Object.entries(values.properties ?? {}).map(([baseKey, value]) => [[name, key, baseKey].filter(Boolean).join('.'), value] as const))),
           new Map(Array.from(dependencies.entries()).map(([key, data]) => [[name, key, data.origin_name_var].filter(Boolean).join('.'), key] as const))
         );
       },
