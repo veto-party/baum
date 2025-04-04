92b4379b770835f94b09db2b0bfbfc3d20c9affd
Timo Scholz <timo.scholz@veto.party>
2025-04-04 17:46:25 +0200

    Remove unused code.

--- steps/installer/renderer/src/implementation/ARendererManager.ts
+++ steps/installer/renderer/src/implementation/ARendererManager.ts
@@ -18,23 +18,6 @@ export abstract class ARendererManager<T extends IFeature<any, any, any>> extend
     super();
   }
 
-  // Create a stable hash from the filter function
-  private static hashFilter(filter: <U extends InferStructure<any>>(obj: U[]) => boolean): string {
-    const serializedFilter = filter.toString(); // Convert filter function to string
-    return createHash('sha256').update(serializedFilter).digest('hex'); // Generate a hash of the function's string
-  }
-
-  private static createFilterKey(filter?: IFilter<any>): string {
-    if (filter === undefined) return 'no_filter';
-    if (typeof filter === 'function') return `func_${ARendererManager.hashFilter(filter)}`;
-    return `str_${filter.key}`;
-  }
-
-  // Create a stable key based on feature and filter
-  private static createFeatureKey<T extends IFeature<any, any, any>>(feature: T, filter?: IFilter<T>, custom?: string): string {
-    return [Object.getPrototypeOf(feature).name, ARendererManager.createFilterKey(filter), custom].filter(Boolean).join('_');
-  }
-
   protected abstract createSelf<U extends IFeature<any, any, any>>(feature: U): ARendererManager<U>;
   protected abstract createFeatureManager(feature: T): IRendererFeatureManager<T>;
 
