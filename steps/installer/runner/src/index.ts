d05347a33b671f3595b9eae9ce8339ca62af3e4d
Timo Scholz <timo.scholz@veto.party>
2025-07-27 07:17:06 +0200

    Linting.

--- steps/installer/runner/src/index.ts
+++ steps/installer/runner/src/index.ts
@@ -1,5 +1,3 @@
-
-export type { ISearchStrategy, ISearchStrategyResult } from "./search/ISearchStrategy.js";
-export { DefaultSearchStrategy } from "./search/implementation/Default/DefaultSearchStragegy.js";
-export { InstallerRunner } from "./InstallerRunner.js";
-
+export { InstallerRunner } from './InstallerRunner.js';
+export type { ISearchStrategy, ISearchStrategyResult } from './search/ISearchStrategy.js';
+export { DefaultSearchStrategy } from './search/implementation/Default/DefaultSearchStragegy.js';
