9fbe82eb32e8018375f7089455e20af87a4ebac9
Timo Scholz <timo.scholz@veto.party>
2025-08-24 19:02:32 +0200

    Ordered imports.

--- registry/cache/storage/package/src/index.ts
+++ registry/cache/storage/package/src/index.ts
@@ -1,6 +1,5 @@
-export { NPMPackageProvider } from './NPMPackageProvider.js';
-export { NPMVersionStrategy } from './NPMVersionStragety.js';
-export { NPMPackageStorage } from './NPMPackageStorage.js';
-
 export { NPMPackageGetter } from './access/NPMPackageGetter.js';
 export { NPMPackageWriter } from './access/NPMPackageWriter.js';
+export { NPMPackageProvider } from './NPMPackageProvider.js';
+export { NPMPackageStorage } from './NPMPackageStorage.js';
+export { NPMVersionStrategy } from './NPMVersionStragety.js';
