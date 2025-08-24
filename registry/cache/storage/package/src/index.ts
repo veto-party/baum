5b7f0c44896892d954e1076a705645bc1eb9ba0c
Timo Scholz <timo.scholz@veto.party>
2025-08-24 19:01:02 +0200

    Update index.ts

--- registry/cache/storage/package/src/index.ts
+++ registry/cache/storage/package/src/index.ts
@@ -1,2 +1,6 @@
 export { NPMPackageProvider } from './NPMPackageProvider.js';
 export { NPMVersionStrategy } from './NPMVersionStragety.js';
+export { NPMPackageStorage } from './NPMPackageStorage.js';
+
+export { NPMPackageGetter } from './access/NPMPackageGetter.js';
+export { NPMPackageWriter } from './access/NPMPackageWriter.js';
