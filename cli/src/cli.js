6e83a2c06ee7b35459879d42db302cfe497f8c22
Timo Scholz <timo.scholz@veto.party>
2025-08-26 21:10:15 +0200

    Update ci to have better error messages

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -28,18 +28,27 @@ if ((process.env.NODE_OPTIONS || '').includes('--loader ts-node')) {
       process.exit(1);
     }
   } catch (error) {
-    console.error(error);
+    console.error("error", error, Error.captureStackTrace(error));
+    throw error;
   }
-} else {
-  const childProcess = spawnSync(process.argv[0], process.argv.slice(1), {
-    stdio: 'inherit',
-    env: {
-      ...process.env,
-      TS_NODE_PROJECT: __dirname,
-      NODE_OPTIONS: [process.env.NODE_OPTIONS, '--loader ts-node/esm', '--trace-uncaught', '--trace-warnings'].filter((item) => !!item).join(' ')
-    },
-    windowsHide: true
-  });
 
-  process.exit(childProcess.status ?? (childProcess.error ? 1 : 0));
+  process.exit(0);
 }
+
+
+/**
+ * This code will get executed in js environment only.
+ * If Typescript is detected using the code above, we will not end up here.
+ */
+
+const childProcess = spawnSync(process.argv[0], process.argv.slice(1), {
+  stdio: 'inherit',
+  env: {
+    ...process.env,
+    TS_NODE_PROJECT: __dirname,
+    NODE_OPTIONS: [process.env.NODE_OPTIONS, '--loader ts-node/esm', '--trace-uncaught', '--trace-warnings'].filter((item) => !!item).join(' ')
+  },
+  windowsHide: true
+});
+
+process.exit(childProcess.status ?? (childProcess.error ? 1 : 0));
