293c14b49412336fab136567d54e17ed7473c89c
Timo Scholz <timo.scholz@veto.party>
2025-08-26 23:43:50 +0200

    Removed debugging (can be passed by user).
    Co-authored-by: Kevin <FCQCarryOut@users.noreply.github.com>

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -46,7 +46,7 @@ const childProcess = spawnSync(process.argv[0], process.argv.slice(1), {
   env: {
     ...process.env,
     TS_NODE_PROJECT: __dirname,
-    NODE_OPTIONS: [process.env.NODE_OPTIONS, '--loader ts-node/esm', '--trace-uncaught', '--trace-warnings'].filter((item) => !!item).join(' ')
+    NODE_OPTIONS: [process.env.NODE_OPTIONS, '--loader ts-node/esm'].filter((item) => !!item).join(' ')
   },
   windowsHide: true
 });
