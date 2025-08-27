df47dbe9e1e033afa2ec92872ac9500e112479e7
Timo Scholz <timo.scholz@veto.party>
2025-08-27 10:25:23 +0200

    Try smth.

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -6,6 +6,26 @@ import * as util from 'node:util';
 
 const __dirname = fileURLToPath(Path.dirname(Path.dirname(import.meta.resolve('baum'))));
 
+if (!(process.env.NODE_OPTIONS || '').includes('--loader ts-node')) {
+
+  /**
+   * This code will get executed in js environment only.
+   * If Typescript is detected using the code above, we will not end up here.
+   */
+
+  const childProcess = spawnSync(process.argv[0], process.argv.slice(1), {
+    stdio: 'inherit',
+    env: {
+      ...process.env,
+      TS_NODE_PROJECT: __dirname,
+      NODE_OPTIONS: [process.env.NODE_OPTIONS, '--loader ts-node/esm'].filter((item) => !!item).join(' ')
+    },
+    windowsHide: true
+  });
+
+  process.exit(childProcess.status ?? (childProcess.error ? 1 : 0));  
+}
+
 process.on('uncaughtException', (error) => {
   try {
     console.error(error[util.inspect.custom](error));
@@ -35,23 +55,4 @@ async function main() {
   process.exit(0);
 }
 
-if ((process.env.NODE_OPTIONS || '').includes('--loader ts-node')) {
-  main();
-}
-
-/**
- * This code will get executed in js environment only.
- * If Typescript is detected using the code above, we will not end up here.
- */
-
-const childProcess = spawnSync(process.argv[0], process.argv.slice(1), {
-  stdio: 'inherit',
-  env: {
-    ...process.env,
-    TS_NODE_PROJECT: __dirname,
-    NODE_OPTIONS: [process.env.NODE_OPTIONS, '--loader ts-node/esm'].filter((item) => !!item).join(' ')
-  },
-  windowsHide: true
-});
-
-process.exit(childProcess.status ?? (childProcess.error ? 1 : 0));
+main();
