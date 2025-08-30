6de09ec24aa33631ef12b3b7e2d3795d87646346
Timo Scholz <timo.scholz@veto.party>
2025-08-30 17:00:15 +0200

    Linting.

--- steps/docker/src/index.ts
+++ steps/docker/src/index.ts
@@ -1,6 +1,6 @@
+export { ADockerStep } from './implementation/ADockerStep.js';
 export { DockerBuildStep } from './implementation/DockerBuildStep.js';
 export { DockerLifecycleStep } from './implementation/DockerLifecycleStep.js';
 export { DockerPushStep } from './implementation/DockerPushStep.js';
 export { DockerRunStep } from './implementation/DockerRunStep.js';
 export { DockerStopStep } from './implementation/DockerStopStep.js';
-export { ADockerStep } from './implementation/ADockerStep.js';
